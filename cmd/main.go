64d09c22233eeb435fd989577f25e31a0490d48d
Alain Sst <AlainLuu.1999@gmail.com>
2022-01-09 14:32:21 +0700

    update code

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,78 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+
+	"net/http"
+	"strings"
+	"test/handler"
+	"test/repository"
+	"test/service"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	initTimeZone()
+	initConfig()
+	db := initDatabase()
+
+	customerRepositoryDB := repository.NewCustomerRepositoryDB(db)
+	customerService := service.NewCustomerService(customerRepositoryDB)
+	customerHandler := handler.NewCustomerHandler(customerService)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
+
+
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/mock v1.6.0
+	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/spf13/viper v1.10.1
 )
